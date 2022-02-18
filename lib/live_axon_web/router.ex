de44bce02da22f9b457db9d03535797d240ac1c3
thehaigo <sirannha@gmail.com>
2022-02-19 04:31:01 +0900

    add page live

--- lib/live_axon_web/router.ex
+++ lib/live_axon_web/router.ex
@@ -17,7 +17,7 @@ defmodule LiveAxonWeb.Router do
   scope "/", LiveAxonWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live "/", PageLive
   end
 
   # Other scopes may use custom stacks.
